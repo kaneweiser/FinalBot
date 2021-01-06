78cbdfe948e000d07262867517d7600ee479776b
kaneweiser <76998938+kaneweiser@users.noreply.github.com>
2021-01-06 18:39:22 +0700

    Initial commit

---
+++ AkenoHimejimabot/modules/fun_strings.py
@@ -0,0 +1,321 @@
+ARAGIFS = [
+         'CgACAgUAAx0CTCs8lgABAg1KX6e6iLkD3UVmuqDXJKWg8C1dljwAArgBAAK0pThVvwOSjYhlMsQeBA',
+         'CgACAgUAAx0CTCs8lgABAg8eX6fCo-C_5jepnqtlEljgbNuXbnsAAtABAAL8LUFVgFSPB8jG874eBA',
+          'CgACAgUAAx0CTCs8lgABAhAXX6fIxIEa1yx5BrOkmuqggwAB0ePMAAL-AQACCXk5Vc8MzDnCRP7xHgQ'
+]
+
+DEATHGIF = [
+         'CgACAgQAAxkBAAIVYF-8xEe_VTpV64Y2KrlJpyiOZ5XQAAKIAgAC1g2cUngIWCy4keBVHgQ',
+         'CgACAgQAAxkBAAIVY1-8xIot4JgiRorWFn23XzzxWWPeAAJnAgAChmbMU3sPMmmrVBViHgQ',
+         'CgACAgQAAxkBAAIVZl-8xOXQdlnoxitp-FnVk3YKLKLlAAJqAgACAdWdUkdW6XodFVDmHgQ',
+         'CgACAgQAAxkBAAIVaV-8xSAIOZZQ8ipSe1d-8pFdLn_3AAIaAgACeSmVUmjbXZv8-QU5HgQ',
+         'CgACAgQAAxkBAAIVbF-8xaNFj64lyQLqGzC42OBjR5nMAAJ3AgAC7F-UUklCkwQvihFRHgQ',
+         'CgACAgQAAxkBAAIVbV-8xbx8ApvUXKGIQSF-57vFeURSAAJlAgACTs2lUn0i--kTmIndHgQ',
+         'CgACAgQAAxkBAAIVbl-8xcUOOEQ2vAO8OMdq3FxeSzeuAAITAgACeImtUq5Wu_WWgi2LHgQ',
+         'CgACAgQAAxkBAAIVel-8xhNRYR4f-5Z-OBoklP1sioNFAAK3AgACH629UQiGai1gP6BwHgQ',
+         'CgACAgQAAxkBAAIVfV-8xlU7GoIXi9miynEdIJYcouvDAAIoAgAChja1UqZXtQABXQ5Trh4E',
+         'CgACAgQAAxkBAAIVgF-8xoLyjW22C6msT83ZUrCp5fGAAAJ2AgACTnTEU5p58iyb02bDHgQ'
+]         
+
+RUN_STRINGS = (
+    "Now you see me, now you don't."
+    "ε=ε=ε=ε=┌(;￣▽￣)┘",
+    "Get back here!",
+    "REEEEEEEEEEEEEEEEEE!!!!!!!",
+    "Look out for the wall!",
+    "Don't leave me alone with them!!",
+    "You've got company!",
+    "Chotto matte!",
+    "Yare yare daze",
+    "*Naruto run activated*",
+    "*Nezuko run activated*",
+    "Hey take responsibilty for what you just did!",
+    "May the odds be ever in your favour.",
+    "Run everyone, they just dropped a bomb 💣💣",
+    "And they disappeared forever, never to be seen again.",
+    "Legend has it, they're still running.",
+    "Hasta la vista, baby.",
+    "Ah, what a waste. I liked that one.",
+    "As The Doctor would say... RUN!",
+)
+
+GIFS = [
+    'CgACAgQAAx0CSVUvGgAC7KpfWxMrgGyQs-GUUJgt-TSO8cOIDgACaAgAAlZD0VHT3Zynpr5nGxsE',
+    'CgACAgUAAx0CU_rCTAABAjdgX1s4NVaeCls6YaH3p43vgdCRwQIAAqsAA4P_MFUYQhyoR-kgpRsE',
+    'CgACAgUAAx0CU_rCTAABAjdSX1s3fq5iEJ64YeQLKI8cD7CSuSEAAlUBAAJu09hW5iqWB0hTPD4bBA'
+]
+
+SLAP_SAITAMA_TEMPLATES = (
+    "Slap me one more time and I'll mute you.",
+    "Stop slapping me. REEEEEEEEEEEEEE.",
+    [
+        "I am muting you for a minute.",  # normal reply
+        "Stop slapping me just because I can't mute you. REEEEEEEEEE.",  # reply to admin
+        "tmute",  # command
+        "Shut up!",
+        "Silence!"
+    ])
+
+SLAP_TEMPLATES = (
+    "{user2} was killed by magic.",
+    "{user2} starved without pats.",
+    "{user2} was knocked into the void by {user1}.",
+    "{user2} fainted.",
+    "{user2} is out of usable Pokemon! {user2} whited out!.",
+    "{user2} is out of usable Pokemon! {user2} blacked out!.",
+    "{user2} got rekt.",
+    "{user2}'s melon was split by {user1}.",
+    "{user2} was sliced and diced by {user1}.",
+    "{user2} played hot-potato with a grenade.",
+    "{user2} was knifed by {user1}.",
+    "{user2} ate a grenade.",
+    "{user2} is what's for dinner!",
+    "{user2} was terminated by {user1}.",
+    "{user1} spammed {user2}'s email.",
+    "{user1} RSA-encrypted {user2} and deleted the private key.",
+    "{user1} put {user2} in the friendzone.",
+    "{user1} slaps {user2} with a DMCA takedown request!",
+    "{user2} got a house call from Doctor {user1}.",
+    "{user1} beheaded {user2}.",
+    "{user2} got stoned...by an angry mob.",
+    "{user1} sued the pants off {user2}.",
+    "{user2} was one-hit KO'd by {user1}.",
+    "{user1} sent {user2} down the memory hole.",
+    "{user2} was a mistake. - '{user1}' ",
+    "{user2} was made redundant.",
+    "{user1} {hits} {user2} with a bat!.",
+    "{user1} {hits} {user2} with a Taijutsu Kick!.",
+    "{user1} {hits} {user2} with X-Gloves!.",
+    "{user1} {hits} {user2} with a Jet Punch!.",
+    "{user1} {hits} {user2} with a Jet Pistol!.",
+    "{user1} {hits} {user2} with a United States of Smash!.",
+    "{user1} {hits} {user2} with a Detroit Smash!.",
+    "{user1} {hits} {user2} with a Texas Smash!.",
+    "{user1} {hits} {user2} with a California Smash!.",
+    "{user1} {hits} {user2} with a New Hampshire Smash!.",
+    "{user1} {hits} {user2} with a Missouri Smash!.",
+    "{user1} {hits} {user2} with a Carolina Smash!.",
+    "{user1} {hits} {user2} with a King Kong Gun!.",
+    "{user1} {hits} {user2} with a baseball bat - metal one.!.",
+    "*Serious punches {user2}*.",
+    "*Normal punches {user2}*.",
+    "*Consecutive Normal punches {user2}*.",
+    "*Two Handed Consecutive Normal Punches {user2}*.",
+    "*Ignores {user2} to let them die of embarassment*.",
+    "*points at {user2}* What's with this sassy... lost child?.",
+    "*Hits {user2} with a Fire Tornado*.",
+    "{user1} pokes {user2} in the eye !",
+    "{user1} pokes {user2} on the sides!",
+    "{user1} pokes {user2}!",
+    "{user1} pokes {user2} with a needle!",
+    "{user1} pokes {user2} with a pen!",
+    "{user1} pokes {user2} with a stun gun!",
+    "{user2} is secretly a Furry!",
+    "Hey Everybody! {user1} is asking me to be mean!",
+    "( ･_･)ﾉ⌒●~* (･.･;) <-{user2}",
+    "Take this {user2}\n(ﾉﾟДﾟ)ﾉ ))))●~* ",
+    "Here {user2} hold this\n(｀・ω・)つ ●~＊",
+    "( ・_・)ノΞ●~*  {user2}\nDieeeee!!.",
+    "( ・∀・)ｒ鹵~<≪巛;ﾟДﾟ)ﾉ\n*Bug sprays {user2}*.",
+    "( ﾟДﾟ)ﾉ占~<巛巛巛.\n-{user2} You pest!",
+    "( う-´)づ︻╦̵̵̿╤── \(˚☐˚”)/ {user2}.",
+    "{user1} {hits} {user2} with a {item}.",
+    "{user1} {hits} {user2} in the face with a {item}.",
+    "{user1} {hits} {user2} around a bit with a {item}.",
+    "{user1} {throws} a {item} at {user2}.",
+    "{user1} grabs a {item} and {throws} it at {user2}'s face.",
+    "{user1} launches a {item} in {user2}'s general direction.",
+    "{user1} starts slapping {user2} silly with a {item}.",
+    "{user1} pins {user2} down and repeatedly {hits} them with a {item}.",
+    "{user1} grabs up a {item} and {hits} {user2} with it.",
+    "{user1} ties {user2} to a chair and {throws} a {item} at them.",
+    "{user1} gave a friendly push to help {user2} learn to swim in lava.",
+    "{user1} bullied {user2}.",
+    "Nyaan ate {user2}'s leg. *nomnomnom*",
+    "{user1} {throws} a master ball at {user2}, resistance is futile.",
+    "{user1} hits {user2} with an action beam...bbbbbb (ง・ω・)ง ====*",
+    "{user1} ara ara's {user2}.",
+    "{user1} ora ora's {user2}.",
+    "{user1} muda muda's {user2}.",
+    "{user2} was turned into a Jojo reference!",
+    "{user1} hits {user2} with {item}.",
+    "Round 2!..Ready? .. FIGHT!!",
+    "WhoPixel will oof {user2} to infinity and beyond.",
+    "{user2} ate a bat and discovered a new disease.",
+    "{user1} folded {user2} into a paper plane",
+    "{user1} served {user2} some bat soup.",
+    "{user2} was sent to his home, the planet of the apes.",
+    "{user1} kicked {user2} out of a moving train.",
+    "{user2} just killed John Wick’s dog.",
+    "{user1} performed an Avada Kedavra spell on {user2}.",
+    "{user1} subjected {user2} to a fiery furnace.",
+    "Sakura Haruno just got more useful than {user2}",
+    "{user1} unplugged {user2}'s life support.",
+    "{user1} subscribed {user2}' to 5 years of bad internet.",
+    "You know what’s worse than Dad jokes? {user2}!",
+    "{user1} took all of {user2}'s cookies.",
+    "{user2} wa mou.......Shindeiru! - {user1}.",
+    "{user2} lost his race piece!",  #No game no life reference
+    "Shut up {user2}, you are just {user2}.",  #No game no life reference
+    "{user1} hits {user2} with Aka si anse!",  #No game no life reference
+    "@NeoTheKitty scratches {user2}",  #Pixels pet cat - @NeoTheKitty 
+    "Majin buu ate {user2}",  #Dbz
+    "Goblin slayer slays {user2}",  #Goblin Slayer
+)
+
+PAT_TEMPLATES = (
+    "{user1} pats {user2} on the head.",
+    "*gently rubs {user2}'s head*.",
+    "*{user1} mofumofus {user2}'s head*",
+    "*{user1} messes up {user2}'s head*",
+    "*{user1} intensly rubs {user2}'s head*",
+    "*{user2}'s waifu pats their head*",
+    "*{user2}'s got free headpats*",
+    "No pats for {user2}!",
+    "Oh no! We are all out of pats.",
+    "This is a designated no pat zone!",
+    "No pats for {user2}!",
+    "{user1} spoils {user2} with headpats!",
+    "{user2} received one free headpat!",
+    "{user1} headpats {user2} whilst giving a lap pillow",
+    "{user1} aggressively pats {user2}",
+    "{user1} gently strokes {user2}'s head",
+    "Pat, pat, pat, pat",
+    "{user2} could not escape {user1}'s headpats",
+    "{user2}.exe has stopped working",
+    "{user1} rubs {user2} on the neck",
+    "Must... extort... HEADPATS",
+    "{user1} headpats {user2}'s head with a pat",
+    "{user1} pats {user2} unexpectedly",
+    "{user1} pats {user2} with consent, maybe?",
+    "Pat pat, {user2} honto kawaii ne!",
+    "{user1} headpats {user2} at 420apm",
+    "{user1} bellyrubs {user2}",
+    "{user1} pats {user2} friendlily",
+    "{user2} uses HEADPATS? O KAWAII KOTO",
+    "*headpats.gif intensifies for {user2}*",
+    "(*´ω´(*｀ω｀)",
+    "(ｏ・_・)ノ”(ᴗ_ ᴗ。)",
+    "(*￣▽￣)ノ”(- -*)",
+    "(っ´ω`)ﾉ(╥ω╥)",
+    "( ´Д｀)ﾉ(´･ω･`) ﾅﾃﾞﾅﾃﾞ",
+)
+
+PAT_GIFS = (
+    "CgACAgQAAxkBAALRX19Xs7tBdOH1gQwS_rglVRkTbgVYAAKEAgACmQn9UWlyGa_xy9_aGwQ",
+    "CgACAgEAAxkBAALRYF9Xs8EnhsDfDpld3ILoqTbzDmwxAAJFAAOJxjlHECanwn69E5QbBA")
+
+PAT_STICKERS = (
+    "CAACAgQAAxkBAALRWV9Xs4HH0XaXfhZe-jWaZoXfs-IsAAJYAwACdDgSEHYOt4KvL02oGwQ",
+    "CAACAgQAAxkBAALRXF9Xs6XmIeDbnoL1wiDky0TdX-CvAAKKAQAC1TMzC9A3CtiT2rqVGwQ")
+
+PING_STRING = (
+    "PONG!!",
+    "I am here!",
+)
+
+ITEMS = (
+    "cast iron skillet",
+    "angry meow",
+    "cricket bat",
+    "wooden cane",
+    "shovel",
+    "toaster",
+    "book",
+    "laptop",
+    "rubber chicken",
+    "spiked bat",
+    "heavy rock",
+    "chunk of dirt",
+    "ton of bricks",
+    "rasengan",
+    "spirit bomb",
+    "100-Type Guanyin Bodhisattva",
+    "rasenshuriken",
+    "Murasame",
+    "ban",
+    "chunchunmaru",
+    "Kubikiribōchō",
+    "rasengan",
+    "spherical flying kat",
+)
+
+THROW = (
+    "throws",
+    "flings",
+    "chucks",
+    "hurls",
+)
+
+HIT = (
+    "hits",
+    "whacks",
+    "slaps",
+    "smacks",
+    "bashes",
+    "pats",
+)
+
+EYES = [['⌐■', '■'], [' ͠°', ' °'], ['⇀', '↼'], ['´• ', ' •`'], ['´', '`'],
+        ['`', '´'], ['ó', 'ò'], ['ò', 'ó'], ['⸌', '⸍'], ['>',
+                                                         '<'], ['Ƹ̵̡', 'Ʒ'],
+        ['ᗒ', 'ᗕ'], ['⟃', '⟄'], ['⪧', '⪦'], ['⪦', '⪧'], ['⪩', '⪨'], ['⪨', '⪩'],
+        ['⪰', '⪯'], ['⫑', '⫒'], ['⨴', '⨵'], ['⩿', '⪀'], ['⩾', '⩽'], ['⩺', '⩹'],
+        ['⩹', '⩺'], ['◥▶', '◀◤'], ['◍', '◎'], ['/͠-', '┐͡-\\'], ['⌣', '⌣”'],
+        [' ͡⎚', ' ͡⎚'], ['≋'], ['૦ઁ'], ['  ͯ'], ['  ͌'], ['ළ'], ['◉'], ['☉'],
+        ['・'], ['▰'], ['ᵔ'], [' ﾟ'], ['□'], ['☼'], ['*'], ['`'], ['⚆'], ['⊜'],
+        ['>'], ['❍'], ['￣'], ['─'], ['✿'], ['•'], ['T'], ['^'], ['ⱺ'], ['@'],
+        ['ȍ'], ['  '], ['  '], ['x'], ['-'], ['$'], ['Ȍ'], ['ʘ'], ['Ꝋ'],
+        [''], ['⸟'], ['๏'], ['ⴲ'], ['◕'], ['◔'], ['✧'], ['■'], ['♥'], [' ͡°'],
+        ['¬'], [' º '], ['⨶'], ['⨱'], ['⏓'], ['⏒'], ['⍜'], ['⍤'], ['ᚖ'], ['ᴗ'],
+        ['ಠ'], ['σ'], ['☯']]
+
+MOUTHS = [['v'], ['ᴥ'], ['ᗝ'], ['Ѡ'], ['ᗜ'], ['Ꮂ'], ['ᨓ'], ['ᨎ'],
+          ['ヮ'], ['╭͜ʖ╮'], [' ͟ل͜'], [' ͜ʖ'], [' ͟ʖ'], [' ʖ̯'], ['ω'], [' ³'],
+          [' ε '], ['﹏'], ['□'], ['ل͜'], ['‿'], ['╭╮'], ['‿‿'], ['▾'], ['‸'],
+          ['Д'], ['∀'], ['!'], ['人'], ['.'], ['ロ'], ['_'], ['෴'], ['ѽ'], ['ഌ'],
+          ['⏠'], ['⏏'], ['⍊'], ['⍘'], ['ツ'], ['益'], ['╭∩╮'], ['Ĺ̯'], ['◡'],
+          [' ͜つ']]
+
+EARS = [
+    ['q', 'p'],
+    ['ʢ', 'ʡ'],
+    ['⸮', '?'],
+    ['ʕ', 'ʔ'],
+    ['ᖗ', 'ᖘ'],
+    ['ᕦ', 'ᕥ'],
+    ['ᕦ(', ')ᕥ'],
+    ['ᕙ(', ')ᕗ'],
+    ['ᘳ', 'ᘰ'],
+    ['ᕮ', 'ᕭ'],
+    ['ᕳ', 'ᕲ'],
+    ['(', ')'],
+    ['[', ']'],
+    ['¯\\_', '_/¯'],
+    ['୧', '୨'],
+    ['୨', '୧'],
+    ['⤜(', ')⤏'],
+    ['☞', '☞'],
+    ['ᑫ', 'ᑷ'],
+    ['ᑴ', 'ᑷ'],
+    ['ヽ(', ')ﾉ'],
+    ['\\(', ')/'],
+    ['乁(', ')ㄏ'],
+    ['└[', ']┘'],
+    ['(づ', ')づ'],
+    ['(ง', ')ง'],
+    ['⎝', '⎠'],
+    ['ლ(', 'ლ)'],
+    ['ᕕ(', ')ᕗ'],
+    ['(∩', ')⊃━☆ﾟ.*'],
+]
+
+TOSS = (
+    "Heads",
+    "Tails",
+)
+
+DECIDE = ("Yes.", "No.", "Maybe.")
+
+TABLE = ("(╯°□°）╯彡 ┻━┻", "I ran out of tables, will order more.",
+         "Go do some work instead of flippin tables.")
---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m AkenoHimejimabot
+web: python3 -m AkenoHimejimabot
+ps:scale worker=1
