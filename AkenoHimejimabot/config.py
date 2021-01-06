184c8885d930e4a22bf270eb3243173fcd2b1ef4
kaneweiser <76998938+kaneweiser@users.noreply.github.com>
2021-01-06 18:47:18 +0700

    Create config.py

---
+++ AkenoHimejimabot/config.py
@@ -0,0 +1,12 @@
+from AkenoHimejimabot.sample_config import Config
+
+class Development(Config):
+    OWNER_ID = 1364874872  # your telegram ID
+    OWNER_USERNAME = "no_phd"  # your telegram username
+    API_KEY = "1422638635:AAEIFWpaB_IjG9cWSwmQ-0D8L9AWsXMcCMA"  # your api key, as provided by the @botfather
+    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/database'  # sample db credentials
+    JOIN_LOGGER = '-1001215912907' # some group chat that your bot is a member of
+    USE_JOIN_LOGGER = True
+    DRAGONS = [275257858, 1447345851]  # List of id's for users which have sudo access to the bot.
+    LOAD = []
+    NO_LOAD = ['translation']
