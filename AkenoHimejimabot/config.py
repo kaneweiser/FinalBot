6f4e8da626fac035ececd9cf7ebd48fe301a416c
kaneweiser <76998938+kaneweiser@users.noreply.github.com>
2021-01-07 20:28:10 +0700

    Update config.py

--- AkenoHimejimabot/config.py
+++ AkenoHimejimabot/config.py
@@ -4,7 +4,7 @@ class Development(Config):
     OWNER_ID = 1364874872  # your telegram ID
     OWNER_USERNAME = "no_phd"  # your telegram username
     API_KEY = "1422638635:AAEIFWpaB_IjG9cWSwmQ-0D8L9AWsXMcCMA"  # your api key, as provided by the @botfather
-     JOIN_LOGGER = '-1001215912907' # some group chat that your bot is a member of
+    JOIN_LOGGER = '-1001215912907' # some group chat that your bot is a member of
     USE_JOIN_LOGGER = True
     DRAGONS = [275257858, 1447345851]  # List of id's for users which have sudo access to the bot.
     LOAD = []
