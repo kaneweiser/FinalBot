fb10965637c7781b22bf86f6f7cbc43b974dd747
kaneweiser <76998938+kaneweiser@users.noreply.github.com>
2021-01-07 13:30:27 +0700

    Update __init__.py

--- AkenoHimejimabot/__init__.py
+++ AkenoHimejimabot/__init__.py
@@ -107,9 +107,7 @@ updater = tg.Updater(TOKEN, workers=WORKERS)
 
 dispatcher = updater.dispatcher
 
-SUDO_USERS = list(SUDO_USERS)
-WHITELIST_USERS = list(WHITELIST_USERS)
-SUPPORT_USERS = list(SUPPORT_USERS)
+
 
 # Load at end to ensure all prev variables have been set
 from AkenoHimejimabot.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler
